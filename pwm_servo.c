12591e04112d87962fbb8b88fb9e489f3a0e00f8
Ariagildo L <ariagildo01@gmail.com>
2025-02-08 03:28:03 -0300

    configura rotações iniciais e giro contínuo

--- pwm_servo.c
+++ pwm_servo.c
@@ -3,6 +3,8 @@
 #include "hardware/pwm.h"
 
 #define SERVO 13
+#define MAX_LEVEL 2400
+#define MIN_LEVEL 500
 
 void configura_pwm(void){
 
@@ -14,23 +16,41 @@ void configura_pwm(void){
 
     pwm_set_wrap(slice, 19999); //definir o valor de wrap
 
-    pwm_set_gpio_level(SERVO, 1000); //definir o cico de trabalho (duty cycle) do pwm
+    pwm_set_gpio_level(SERVO, 0); //definir o cico de trabalho (duty cycle) do pwm
 
     pwm_set_enabled(slice, true); //habilita o pwm no slice correspondente
 
 }
 
+void inializa_servo(void){
+    pwm_set_gpio_level(SERVO, 1470);
+        sleep_ms(5000);
+        pwm_set_gpio_level(SERVO, 2400);
+        sleep_ms(5000);
+        pwm_set_gpio_level(SERVO, 500);
+        sleep_ms(5000);
+}
+
 int main()
 {
     configura_pwm();
-
-
+    inializa_servo();
+    uint16_t t_ciclo = 500;
+    bool rise = 1;
     while (true) {
-        pwm_set_gpio_level(SERVO, 1470);
-        sleep_ms(1000);
-        pwm_set_gpio_level(SERVO, 2400);
-        sleep_ms(1000);
-        pwm_set_gpio_level(SERVO, 500);
-        sleep_ms(1000);
+        if (rise){
+            for (t_ciclo; t_ciclo < MAX_LEVEL; t_ciclo += 5){
+                sleep_ms(10);
+                pwm_set_gpio_level(SERVO, t_ciclo);
+            }
+            rise = 0;
+        }
+        else {
+            for (t_ciclo; t_ciclo > MIN_LEVEL; t_ciclo -= 5){
+                sleep_ms(10);
+                pwm_set_gpio_level(SERVO, t_ciclo);
+            }
+            rise = 1;
+        }
     }
 }
